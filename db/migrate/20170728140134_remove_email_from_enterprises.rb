34429d243483c6948b70894a07f1b75cc6770a24
Matt-Yorkley <visible_souls@hotmail.com>
2017-09-16 17:31:37 +0100

    Ensure enterprise emails correspond to a user

--- db/migrate/20170728140134_remove_email_from_enterprises.rb
+++ db/migrate/20170728140134_remove_email_from_enterprises.rb
@@ -1,8 +1,14 @@
 class RemoveEmailFromEnterprises < ActiveRecord::Migration
   def up
-    Enterprise.select([:id, :email]).each do |e|
-      contact_user = Spree::User.find_by_email e.email
-      manager = EnterpriseRole.find_or_initialize_by_user_id_and_enterprise_id(contact_user.id, e.id)
+    Enterprise.select([:id, :email]).each do |enterprise|
+      contact_user = Spree::User.find_by_email enterprise.email
+      unless contact_user
+        password = Devise.friendly_token.first(8)
+        contact_user = Spree::User.create(email: enterprise.email, password: password, password_confirmation: password)
+        contact_user.send_reset_password_instructions
+      end
+
+      manager = EnterpriseRole.find_or_initialize_by_user_id_and_enterprise_id(contact_user.id, enterprise.id)
       manager.update_attribute :receives_notifications, true
     end
 
