8149940ff81b7f774e6cf896631d5cc2e8120046
Matt-Yorkley <visible_souls@hotmail.com>
2018-06-08 21:14:32 +0100

    Admin Order guest checkout status

--- app/controllers/spree/admin/orders/customer_details_controller_decorator.rb
+++ app/controllers/spree/admin/orders/customer_details_controller_decorator.rb
@@ -1,7 +1,17 @@
 Spree::Admin::Orders::CustomerDetailsController.class_eval do
+  before_filter :guest_checkout_status, only: :update
+
   # Inherit CanCan permissions for the current order
   def model_class
     load_order unless @order
     @order
+  end
+
+  private
+
+  def guest_checkout_status
+    registered = Spree.user_class.exists?(email: params[:order][:email])
+
+    params[:order][:guest_checkout] = !registered
   end
 end
