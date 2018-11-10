0f2d1af226ea817ff3079850f234c8cecd02a490
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2018-11-10 15:06:38 +0000

    VO specs

--- spec/models/variant_override_spec.rb
+++ spec/models/variant_override_spec.rb
@@ -157,4 +157,106 @@ describe VariantOverride do
   context "extends LocalizedNumber" do
     it_behaves_like "a model using the LocalizedNumber module", [:price]
   end
+
+  context "changing stock levels via orders" do
+    let(:hub) { create(:distributor_enterprise, with_payment_and_shipping: true) }
+    let(:producer) { create(:supplier_enterprise) }
+    let(:oc) { create(:simple_order_cycle, suppliers: [producer], coordinator: hub, distributors: [hub]) }
+    let(:outgoing_exchange) { oc.exchanges.outgoing.first }
+    let(:sm) { hub.shipping_methods.first }
+    let(:pm) { hub.payment_methods.first }
+    let(:p1) { create(:simple_product, supplier: producer) }
+    let(:p2) { create(:simple_product, supplier: producer) }
+
+    let(:v1) { create(:variant, product: p1, count_on_hand: 11) }
+    let(:v2) { create(:variant, product: p1, count_on_hand: 12) }
+    let(:v3) { create(:variant, product: p1, count_on_hand: 13) }
+    let(:v4) { create(:variant, product: p2, count_on_hand: 14) }
+
+    # vo1 overrides stock
+    let!(:vo1) { create(:variant_override, variant: v1, count_on_hand: 21, hub: hub, default_stock: nil, resettable: false) }
+    # vo2 does NOT override stock
+    let!(:vo2) { create(:variant_override, variant: v2, count_on_hand: nil, hub: hub, default_stock: nil, resettable: false) }
+    # vo3 overrides on_demand to true
+    let!(:vo3) { create(:variant_override, variant: v3, count_on_hand: 23, on_demand: true, hub: hub, default_stock: nil, resettable: false) }
+
+    let(:address) { create(:address) }
+
+    before do
+      outgoing_exchange.variants = [v1, v2, v3, v4]
+      create(:mail_method)
+    end
+
+    describe "placing an order" do
+      let(:order) { create(:order, distributor: hub, order_cycle: oc) }
+      let(:li1) { create(:line_item, quantity: 1, order: order, variant: v1) } # VO overrides stock to 21 (from 11)
+      let(:li2) { create(:line_item, quantity: 2, order: order, variant: v2) } # VO does not override stock (from 12)
+      let(:li3) { create(:line_item, quantity: 3, order: order, variant: v3) } # VO overrides stock to 23 (from 13) AND sets on_demand: true
+      let(:li4) { create(:line_item, quantity: 4, order: order, variant: v4) } # This is a simple variant with no overide
+
+      before do
+        order.line_items << [li1, li2, li3, li4]
+        order.shipping_method = create(:shipping_method)
+        order.bill_address = address
+        order.ship_address = address
+      end
+
+      it "correctly decrements stock levels on order completion" do
+        complete_order!
+
+        expect(Spree::Variant.find(v1).count_on_hand).to eq 11 # Ordered 1, stock overridden
+        expect(Spree::Variant.find(v2).count_on_hand).to eq 10 # Ordered 2, stock  NOT overridden
+        expect(Spree::Variant.find(v3).count_on_hand).to eq 13 # Ordered 3, stock overridden and on_demand set
+        expect(Spree::Variant.find(v4).count_on_hand).to eq 10 # Ordered 4, simple variant
+
+        expect(vo1.reload.count_on_hand).to eq 20 # Ordered 1, stock overridden
+        expect(vo2.reload.count_on_hand).to eq nil # Ordered 2, stock NOT overridden
+        expect(vo3.reload.count_on_hand).to eq 23 # Ordered 3, stock overridden and on_demand set
+      end
+
+      it "correctly resets stock levels when an order is cancelled" do
+        complete_order!
+        order.cancel!
+
+        expect(Spree::Variant.find(v1).count_on_hand).to eq 11
+        expect(Spree::Variant.find(v2).count_on_hand).to eq 12
+        expect(Spree::Variant.find(v3).count_on_hand).to eq 13
+        expect(Spree::Variant.find(v4).count_on_hand).to eq 14
+
+        expect(vo1.reload.count_on_hand).to eq 21
+        expect(vo2.reload.count_on_hand).to eq nil
+        expect(vo3.reload.count_on_hand).to eq 23
+      end
+
+      it "does not correctly reset stock levels if variant_overrides are changed, then an order is cancelled" do
+        # An order is placed by a customer of a hub
+        complete_order!
+
+        # Two days later, the hub manager changes their variant overrides
+        vo1.destroy
+        vo2.destroy
+        vo3.destroy
+
+        # The order is cancelled at some time after that point
+        order.reload.cancel!
+
+        # Stock levels of variants belonging to "Producer X" are now incorrectly altered as
+        # a consequence of "Hub Y" updating their inventory and an order being cancelled.
+        # This is because the line_item on the order has no idea if it decremented the variant
+        # from "Producer X", or the variant_override from "Hub Y" at the time the order was placed.
+
+        expect(Spree::Variant.find(v1).count_on_hand).to eq 12 # This variant has been incorrectly restocked
+        expect(Spree::Variant.find(v2).count_on_hand).to eq 12 # The VO didn't the stock in this case, so this is actually correct
+        expect(Spree::Variant.find(v3).count_on_hand).to eq 16 # This variant has been incorrectly restocked
+        expect(Spree::Variant.find(v4).count_on_hand).to eq 14 # Simple variant, fine
+      end
+    end
+  end
+
+  private
+
+  def complete_order!
+    order.state = "complete"
+    order.finalize!
+  end
 end
