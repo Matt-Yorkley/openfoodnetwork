f51c3c962e5951aff0be9d4c24ad4c179c10597b
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-23 18:01:23 +0000

    Add tax category id to adjustments

--- db/schema.rb
+++ db/schema.rb
@@ -372,9 +372,11 @@ ActiveRecord::Schema.define(version: 20210407170804) do
     t.string   "state",           limit: 255
     t.integer  "order_id"
     t.boolean  "included",                                             default: false
+    t.integer  "tax_category_id"
     t.index ["adjustable_type", "adjustable_id"], name: "index_spree_adjustments_on_adjustable_type_and_adjustable_id", using: :btree
     t.index ["order_id"], name: "index_spree_adjustments_on_order_id", using: :btree
     t.index ["originator_type", "originator_id"], name: "index_spree_adjustments_on_originator_type_and_originator_id", using: :btree
+    t.index ["tax_category_id"], name: "index_spree_adjustments_on_tax_category_id", using: :btree
   end
 
   create_table "spree_assets", force: :cascade do |t|
