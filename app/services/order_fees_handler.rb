7d12f11025706cd35b4ef8b149e944b10600dfc3
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-04-13 12:11:41 +0100

    Tax enterprise fee adjustments when calling #recreate_all_fees!

--- app/services/order_fees_handler.rb
+++ app/services/order_fees_handler.rb
@@ -19,6 +19,7 @@ class OrderFeesHandler
 
       create_line_item_fees!
       create_order_fees!
+      tax_enterprise_fees!
 
       order.updater.update_totals
       order.updater.persist_totals
@@ -39,6 +40,10 @@ class OrderFeesHandler
     return unless order_cycle
 
     calculator.create_order_adjustments_for order
+  end
+
+  def tax_enterprise_fees!
+    Spree::TaxRate.adjust(order, order.all_adjustments.enterprise_fee)
   end
 
   def update_line_item_fees!(line_item)
