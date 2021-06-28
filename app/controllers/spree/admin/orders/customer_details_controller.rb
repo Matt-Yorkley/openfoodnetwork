a582f8742f751dc95766054e20e6237f2e6eb7d0
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-06-28 15:30:06 +0100

    Extract comment-method

--- app/controllers/spree/admin/orders/customer_details_controller.rb
+++ app/controllers/spree/admin/orders/customer_details_controller.rb
@@ -25,9 +25,10 @@ module Spree
               @order.associate_user!(Spree.user_class.find_by(email: @order.email))
             end
 
+            refresh_shipment_rates
+
             OrderWorkflow.new(@order).complete
 
-            @order.shipments.map(&:refresh_rates)
             flash[:success] = Spree.t('customer_details_updated')
             redirect_to spree.admin_order_customer_path(@order)
           else
@@ -42,6 +43,10 @@ module Spree
         end
 
         private
+
+        def refresh_shipment_rates
+          @order.shipments.map(&:refresh_rates)
+        end
 
         def order_params
           params.require(:order).permit(
